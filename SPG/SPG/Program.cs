12a2fe27052839ddf597929bc21c8b3ca9b1fb05
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-01-10 19:44:53 -0800

    New console app set up Added the Intro method

---
+++ SPG/SPG/Program.cs
@@ -0,0 +1,30 @@
+﻿   using System;
+
+namespace SPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            //playOneGame();
+            //displayHangman();
+            //createHint();
+            //readGuess();
+            //getRandomWord();
+            //stats();
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine("Welcome to the Guessing game! \n" +
+                              "I will think of a random word. \n" +
+                              "You'll try to guess its letters.\n" +
+                              "Every time you guess a letter \n" +
+                              "that isn't in my word, a new body \n" +
+                              "part of the person appears. \n" +
+                              "Guess correctly to win!"
+                              );
+        }
+    }
+}
